ba1fd387165df08cfc8e8c020b6fe0afab577257
Flowery Snow <fchysan@gmail.com>
2019-11-27 12:14:40 -0600

    testing color changes
    see title.

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -30,14 +30,13 @@ exactly 2 "/mob text paths" '"/mob'
 exactly 10 "/obj text paths" '"/obj'
 exactly 8 "/turf text paths" '"/turf'
 exactly 1 "world<< uses" 'world<<|world[[:space:]]<<'
-exactly 1 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
-exactly 179 "<< uses" '(?<!<)<<(?!<)' -P
+exactly 43 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
+exactly 485 "<< uses" '(?<!<)<<(?!<)' -P
 exactly 0 "incorrect indentations" '^( {4,})' -P
-exactly 26 "text2path uses" 'text2path'
+exactly 27 "text2path uses" 'text2path'
 exactly 3 "update_icon() override" '/update_icon\((.*)\)'  -P
-exactly 1 "goto uses" 'goto '
-exactly 459 "spawn uses" 'spawn\s*\(\s*(-\s*)?\d*\s*\)' -P
-exactly 6 "atom/New uses" '^/(obj|atom|area|mob|turf).*/New\('
+exactly 0 "goto uses" 'goto '
+exactly 526 "spawn uses" 'spawn\s*\(\s*(-\s*)?\d*\s*\)' -P
 # With the potential exception of << if you increase any of these numbers you're probably doing it wrong
 
 broken_files=0
