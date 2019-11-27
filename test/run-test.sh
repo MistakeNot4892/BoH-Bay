4dc069dc9d60aa5e18be0c3aa56cf63af0514a43
Flowery Snow <fchysan@gmail.com>
2019-11-27 12:00:46 -0600

    MD5 Change
    Checking to see if it is travis or local sys issue.

--- test/run-test.sh
+++ test/run-test.sh
@@ -224,7 +224,9 @@ function run_byond_tests {
         ./install-byond.sh || exit 1
         source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
     fi
-    run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH nebula.dme"
+    run_test_ci "check globals build" "python3 tools/GenerateGlobalVarAccess/gen_globals.py baystation12.dme code/_helpers/global_access.dm"
+    run_test "check globals unchanged" "md5sum -c - <<< 'f2b2be13bf069f5ef747c105c5c665cd *code/_helpers/global_access.dm'"
+    run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH baystation12.dme"
     run_test "check no warnings in build" "grep ', 0 warnings' build_log.txt"
     run_test "run unit tests" "DreamDaemon nebula.dmb -invisible -trusted -core 2>&1 | tee log.txt"
     run_test "check tests passed" "grep 'All Unit Tests Passed' log.txt"
